a807edc0e57b3d39eec0973d28f557ddcba9d385
peter115342 <pmuzslay8@gmail.com>
2023-11-11 08:48:45 +0100

    first commit

---
+++ src/components/GraphPage.vue
@@ -0,0 +1,50 @@
+<template>
+   <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Graphs</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Results</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Active Graph</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+    </nav>
+    
+
+  </template>
+  
+  <script>
+  export default {
+    name: 'GraphPage',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  </style>
+  
